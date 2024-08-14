d832feacc6528ad06b7627beaa05b9cb2a73b4f0
sametbaglan <sametbaglan@gonder-al.com>
2024-08-14 15:58:43 +0300

    All layers have been created for the catalog service

---
+++ Catalog.Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CatalogService.Domain.Entities
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation((BsonType.ObjectId))]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
